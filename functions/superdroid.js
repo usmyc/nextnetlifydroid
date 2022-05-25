0e438544ca2d1ec0820c8a6c0f9408d570779d43
usameyuce <avusameyuce@outlook.com>
2022-05-25 13:07:33 +0200

    netlify function

---
+++ functions/superdroid.js
@@ -0,0 +1,8 @@
+exports.handler = async () => {
+  console.log("function ran");
+  const data = { name: "droid", age: "30", job: "robot" };
+  return {
+    statusCode: 200,
+    body: JSON.stringfy(data),
+  };
+};
